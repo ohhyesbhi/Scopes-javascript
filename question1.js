ff8c737a39648fda77cc8a4a2edce0d23cff832c
unknown <kamatabhishek14@gmail.com>
2023-09-27 15:01:30 +0530

    JS in not an interpreted language

---
+++ question1.js
@@ -0,0 +1,11 @@
+//What do you think JS is compiled or interpreted language ? Let's see wheteher it is interpreted or compiled
+
+
+function fun ( ){
+    console...log("i am inside fun");
+    return 0
+}
+
+console.log("welcome")
+fun();
+console...log("end")
