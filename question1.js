327ba93d35a1b59672dda8a62087a78edd46e06e
unknown <kamatabhishek14@gmail.com>
2023-09-27 15:04:36 +0530

    o/p check

--- question1.js
+++ question1.js
@@ -9,3 +9,8 @@ function fun ( ){
 console.log("welcome")
 fun();
 console...log("end")
+
+
+ // o/p -: console...log("i am inside fun"); SyntaxError: Unexpected token '...'
+
+ // if js was interpreted then atleast in o/p we could have got welcome , so hence we can say javascript is not interpreted language
