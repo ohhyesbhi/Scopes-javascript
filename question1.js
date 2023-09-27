ccebdbc3c3996edc75a7159506ef09746a72768a
unknown <kamatabhishek14@gmail.com>
2023-09-27 15:14:15 +0530

    JS is not a compiled language

--- question1.js
+++ question1.js
@@ -1,16 +1,25 @@
 //What do you think JS is compiled or interpreted language ? Let's see wheteher it is interpreted or compiled
 
 
-function fun ( ){
-    console...log("i am inside fun");
-    return 0
-}
+// function fun ( ){
+//     console...log("i am inside fun");
+//     return 0
+// }
 
-console.log("welcome")
-fun();
-console...log("end")
+// console.log("welcome")
+// fun();
+// console...log("end")
 
 
  // o/p -: console...log("i am inside fun"); SyntaxError: Unexpected token '...'
 
  // if js was interpreted then atleast in o/p we could have got welcome , so hence we can say javascript is not interpreted language
+
+
+
+ function fun (){
+    console.log("hello");
+    console.lo("hey")
+ }
+
+ fun()
