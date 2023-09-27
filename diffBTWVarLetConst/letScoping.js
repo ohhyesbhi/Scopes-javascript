7ed9b68b308d80da4c8f7110dc800a493fca3c52
unknown <kamatabhishek14@gmail.com>
2023-09-27 16:14:24 +0530

    added info about let scoping

---
+++ diffBTWVarLetConst/letScoping.js
@@ -0,0 +1,16 @@
+// let -: It actually gives a block scope
+
+let x = 1
+
+if(x == 1){
+    let x = 2;
+    console.log(x)
+}
+
+console.log(x)
+
+// o/p -: 2 , 1
+
+// reason -: The set curly brackets is a block and let gives us block scope that means when we initialise let x = 2 (line no 6) , it means
+//           a new variable x has been initialised  in the visiblity of this block which is DIFFERENT from the x which is initialised at
+//           the line no 3 
