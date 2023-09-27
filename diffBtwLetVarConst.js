6f89a387f8843eeabed2bc067717b9c6a55a5ba5
unknown <kamatabhishek14@gmail.com>
2023-09-27 15:58:49 +0530

    added info about var scoping

---
+++ diffBtwLetVarConst.js
@@ -0,0 +1,17 @@
+// var -: when we use var then whatever variable you are declaring it will either having a function scope or a global scope
+
+// example -: 
+
+var x = 1;
+
+if( x == 1 ){
+    var x = 2;
+    console.log(x)
+}
+
+console.log(x);
+
+// o/p -: 2 , 2
+// reason -: var x = 1 , where x is actually acting as a global variable and the we can see we have a if BLOCK , which is not a function
+//           and now we just read that var is nowhere knowing about the block scoping , that means when we do ( var x = 2 ) it will be 
+//           acting as a global variable x only  
