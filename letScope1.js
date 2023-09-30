004d418c595bbd1b2b9c8f5b3ada741c1dcc5028
unknown <kamatabhishek14@gmail.com>
2023-09-30 13:42:40 +0530

    added another example based of scopes

--- letScope1.js
+++ letScope1.js
@@ -2,7 +2,8 @@ var teacher = "sanket";
 
 function fun(){
 
-    console.log(teacher);       // this is undefined because while execution when we ask hey scope of fun do you have teacher? YES,                                // but the thing is in parsing phase we don't assign the value s to the variable so it shows undefined
+    console.log(teacher);       // this is undefined because while execution when we ask hey scope of fun do you have teacher? YES,
+                                // but the thing is in parsing phase we don't assign the value s to the variable so it shows undefined
 
     var teacher = "abhishek";
     let content = "js";
---
+++ scope8.js
@@ -0,0 +1,13 @@
+
+
+function fun(){
+
+var i = 5 
+while( i < 10 ){
+   var x = i
+   i++
+}
+console.log(x)
+}
+
+fun();
