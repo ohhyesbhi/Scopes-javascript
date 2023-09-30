f226f035990dfef281d469ce3d3fa6c2c132a1c1
unknown <kamatabhishek14@gmail.com>
2023-09-30 19:21:32 +0530

    added example on function expression

---
+++ functionExpression1.js
@@ -0,0 +1,7 @@
+const f = function fun(){
+    console.log("how much fun??")
+}
+
+ 
+f() ;      // o/p -: how much fun ?
+fun();  // o/p -: error , because here function fun get's a scope of corresponding variable in which they are assigned
